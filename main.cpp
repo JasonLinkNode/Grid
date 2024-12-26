f8f84d3d8fdf11e544cad1be3c38fa1c4998d5a1
JasonLinkNode <jasonlinknode@gmail.com>
2024-12-26 14:54:04 +0800

    add main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: main.cpp
+  > Author: Jason
+  > Mail: jasonlinknode@gmail.com
+  > Created Time: Thu 26 Dec 2024 02:53:49 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
